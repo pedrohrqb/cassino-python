4f057812f497e371cfb86924fb9d17bf7ecbd26b
Pedro Henrique <pedrobezerraout@gmail.com>
2025-02-05 12:01:49 -0300

    finish

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from abc import ABC, abstractmethod
 import itertools
 import random
 from time import sleep
+import os
+import matplotlib.pyplot as plt 
+
+class Player:
+    def __init__(self, balance = 0):
+        self.balance = balance
 
 class CassaNiquel:
 
@@ -37,11 +43,13 @@ class CassaNiquel:
         
         return result
 
-    def _display(self, amount_bet, result, time=0.1):
-        seconds = 1
+    def _display(self, amount_bet, result, time=0.3):
+        seconds = 2
         for i in range(0, int(seconds/time)):
             print(self._emojize(random.choice(self.permutations)))
             sleep(time)
+            os.system('cls')
+
         print(self._emojize(result))
 
         if self._check_result_user(result):
@@ -56,9 +64,43 @@ class CassaNiquel:
         x = [result[0] == x for x in result]
         return True if all(x) else False      
 
-    def _update_balance(self, amount)
+    def _update_balance(self, amount_bet, result, player: Player):
+        if self._check_result_user(result):
+            self.balance -= (amount_bet * 3)
+            player.balance += (amount_bet * 3)
+        else:
+            self.balance += amount_bet
+            player.balance -= amount_bet
+    
+    def play(self, amount_bet, player: Player):
+        result = self._get_final_result()
+      #  self._display(amount_bet, result)
+        self._update_balance(amount_bet, result, player)
 
+    @property    
+    def gain(self):
+        return self.initial_balance + self.balance
 
+maquina1 = CassaNiquel(level=3)
 
-maquina1 = CassaNiquel()
-maquina1._display(0, maquina1._get_final_result())
+JOGADORES_POR_DIA = 100
+APOSTAS_POR_DIA = 5
+DIAS = 180
+VALOR_MAXIMO = 200
+
+saldo = []
+players = [Player() for i in range(JOGADORES_POR_DIA)]
+
+for i in range(0, DIAS):
+    for j in players:
+        for k in range(0, random.randint(1, APOSTAS_POR_DIA)):
+            maquina1.play(random.randint(5, VALOR_MAXIMO), j)
+    saldo.append(maquina1.gain)
+
+plt.figure()
+x = [i for i in range(1, DIAS+1)]
+plt.plot(x, saldo)
+plt.show()
+
+plt.plot([i for i in range(JOGADORES_POR_DIA)], [i.balance for i in players])
+plt.show()
