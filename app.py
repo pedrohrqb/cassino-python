8901e2f3deb144db23965efce55269b6bea689c3
Pedro Henrique <pedrobezerraout@gmail.com>
2025-02-05 11:12:47 -0300

    update

--- app.py
+++ app.py
@@ -1,45 +1,11 @@
 from abc import ABC, abstractmethod
 import itertools
 import random
-
-
-def BaseMachine(ABC):
-    @abstractmethod
-    def _gen_permutations(self):
-        pass
-
-    @abstractmethod
-    def _get_final_result(self):
-        pass
-    
-    @abstractmethod
-    def _display(self):
-        pass
-
-    @abstractmethod
-    def _check_result_user(self):
-        pass
-
-    @abstractmethod
-    def _update_balance(self):
-        pass    
-
-    @abstractmethod
-    def emojize(self):
-        pass
-
-    @abstractmethod
-    def gain(self):
-        pass
-
-    @abstractmethod
-    def play(self, amount_bet, player):
-        pass
-
+from time import sleep
 
 class CassaNiquel:
 
-    def __init__(self, level=1):
+    def __init__(self, level=1, balance=0):
         self.SIMBOLOS = {
             'money_mouth_face': '1F911',
             'cold_face': '1F976',
@@ -49,7 +15,10 @@ class CassaNiquel:
         }
         self.level = level
         self.permutations = self._gen_permutations()
-    
+        self.balance = 0
+        self.initial_balance = self.balance
+
+
     def _gen_permutations(self):
         permutations = list(itertools.product(self.SIMBOLOS.keys(), repeat=3))
         for j in range(self.level):
@@ -65,8 +34,31 @@ class CassaNiquel:
         
         if len(set(result)) == 3 and random.randint(0, 5) >= 2:
             result[1] = result[0]
-
         
+        return result
+
+    def _display(self, amount_bet, result, time=0.1):
+        seconds = 1
+        for i in range(0, int(seconds/time)):
+            print(self._emojize(random.choice(self.permutations)))
+            sleep(time)
+        print(self._emojize(result))
+
+        if self._check_result_user(result):
+            print(f'Você venceu e recebeu: {amount_bet*3}')
+        else:
+            print('foi quase, tente novamente.')
+
+    def _emojize(self, emojis):
+        return ''.join(tuple(chr(int(self.SIMBOLOS[code], 16)) for code in emojis))
+
+    def _check_result_user(self, result):
+        x = [result[0] == x for x in result]
+        return True if all(x) else False      
+
+    def _update_balance(self, amount)
+
+
 
 maquina1 = CassaNiquel()
-maquina1._get_final_result()
+maquina1._display(0, maquina1._get_final_result())
